5c54b7bd523c459899a7c49fa1c979fddcf9e276
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 16:51:19 +0700

    login usecase

--- auth_service/src/usecase/authUseCase.js
+++ auth_service/src/usecase/authUseCase.js
@@ -31,10 +31,49 @@ class AuthUseCase {
 
         userData.password = this.bcrypt.hashSync(userData.password, 10);
         let userRegister = await this._userRepository.createUser(userData);
-        
+
         result.isSuccess = true;
         result.statusCode = 200;
         result.data = userRegister
+        return result;
+      }
+
+
+      async login(userdata) {
+        let result = {
+          isSuccess: false,
+          statusCode: 404,
+          reason: null,
+          data: null,
+        };
+        const user = await this._authRepository.getUserByUsernameOrMsisdn(user.usernameOrMsisdn);
+        if (user === null) {
+          result.reason = 'username or password incorect';
+          result.statusCode = 404;
+          return result;
+        }
+        const comparePassword = await this._bcrypt.compareSync(userdata.password, user.password);
+    
+        if (comparePassword === null) {
+          result.reason = 'username and password incorrect';
+          result.statusCode = 400;
+          return result;
+        }
+    
+        const userObj = {
+          id: user.id,
+          msisdn: user.msisdn,
+          name: user.name,
+          username: user.username,
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+        };
+        const tokenManager = await this._tokenManager.generateToken(userObj);
+    
+        result.isSuccess = true;
+        result.statusCode = 200;
+        result.data = {userObj, tokenManager};
+    
         return result;
       }
 }
