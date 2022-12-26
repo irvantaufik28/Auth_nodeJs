9c8e2554a8be7219e44f69eeef43320a4e367a7b
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 15:11:23 +0700

    register usecase

--- auth_service/src/app.js
+++ auth_service/src/app.js
@@ -3,11 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors')
 const serverError = require('./middlerware/serverError')
+const bcrypt = require('bcrypt');
+const tokenManager = require('./helper/tokenManager')
      
 
-const app = express();
+const UserRepository = require('./repository/userRepository')
+const AuthUseCase = require('./usecase/authUseCase')
 
+const AuthUC = new UserRepository(new AuthUseCase(), bcrypt, tokenManager)
+
+
+const app = express();
 app.use((req, res, next) => {
+    req.AuthUC = AuthUC;
 
   next();
 });
---
+++ auth_service/src/usecase/authUseCase.js
@@ -0,0 +1,43 @@
+class AuthUseCase {
+    constructor(userRepository, bcrypt, tokenManager) {
+      this._userRepository = userRepository;
+      this._bcrypt = bcrypt;
+      this._tokenManager = tokenManager;
+    }
+    async register(userData) {
+        let result = {
+          isSuccess: false,
+          reason: "",
+          statusCode: 400,
+          data: null,
+          token: null,
+        };
+        
+        if (userData.password !== userData.confirmPassword) {
+          result.reason = "password and confirm password not match";
+          return result;
+        }
+        const verifyUsername = await this._userRepository.getUserByUsernameOrMsisdn(userData.username)
+        const verifyMsisdn = await this._userRepository.getUserByUsernameOrMsisdn(userData.msisdn)
+        if (verifyUsername !== null) {
+          result.reason = "username is existing";
+          return result;
+        } 
+        if (verifyMsisdn !== null) {
+            result.reason = "msisdn is existing";
+            return result;
+        }
+
+        userData.password = this.bcrypt.hashSync(userData.password, 10);
+        let userRegister = await this.authRepo.register(userData);
+
+
+        result.isSuccess = true;
+        result.statusCode = 200;
+        result.data = userRegister
+        return result;
+      }
+}
+  
+  
+  module.exports = AuthUseCase;
