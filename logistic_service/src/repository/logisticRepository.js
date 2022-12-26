03257b5500e6eb8d02daa1b3f08971cbc30d7d12
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 18:55:20 +0700

    repository logistic

---
+++ logistic_service/src/repository/logisticRepository.js
@@ -0,0 +1,44 @@
+const { Logistic } = require("../models");
+
+class LogisticRepository {
+  constructor() {
+    this._LogisticRepository = Logistic;
+  }
+
+  async getAll() {
+    const result = await this._LogisticRepository.findAll();
+    return result;
+  }
+
+  async getById(id) {
+    const result = await this._LogisticRepository.findOne({
+      where: { id },
+    });
+    return result;
+  }
+  
+  async create(logistic) {
+    const result = await this._LogisticRepository.create(logistic);
+    return result;
+  }
+
+  async update(logistic, id) {
+    const result = await this._LogisticRepository.update(logistic, {
+      where: {
+        id,
+      },
+    });
+    return result;
+  }
+
+  async delete(di) {
+    const result = await this._LogisticRepository.delete({
+      where: {
+        id,
+      },
+    });
+    return result;
+  }
+}
+
+module.exports = LogisticRepository;
