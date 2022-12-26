e157dc42a3959e34ac2e116f7b70997df0a5f5c5
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 16:53:01 +0700

    login controller

--- auth_service/src/controller/authController.js
+++ auth_service/src/controller/authController.js
@@ -21,5 +21,24 @@ module.exports = {
     } catch (error) {
       next(error);
     }
+  }, 
+
+  login: async (req, res, next) => {
+    try {
+      const user = {
+        username_or_msisdn: req.body.username_or_msisdn,
+        password: req.body.password,
+      };
+
+      const result = await req.authUC.login(user);
+
+      if (!result.isSuccess) {
+        return res.status(result.statusCode).json(resData.failed(result.reason));
+      }
+
+      res.status(result.statusCode).json(resData.success(result.data));
+    } catch (error) {
+      next(error);
+    }
   },
 };
