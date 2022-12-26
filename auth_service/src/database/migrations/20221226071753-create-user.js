c7d59e719c25829b3bb3b1957a43df57c28e4c78
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 14:37:45 +0700

    generate table user

---
+++ auth_service/src/database/migrations/20221226071753-create-user.js
@@ -0,0 +1,37 @@
+'use strict';
+const { v4: UUIDV4 } = require('uuid');
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: UUIDV4
+      },
+      msisdn: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
