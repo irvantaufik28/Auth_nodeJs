e3f90b50fedd698a5d5fc332bacb7a423ba7fe84
irvan taufik <irvantaufik28@gmail.com>
2022-12-27 06:31:21 +0700

    router logistic

---
+++ logistic_service/src/routes/courierRateRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const courierRateController = require('../controller/courierRateController.js');
+const authorized = require('../middlerware/authorization');
+
+
+const router = express.Router();
+router.get('/courier-rate', authorized, courierRateController.getAllCourierRate);
+router.get('/courier-rate-by-origin-dest', authorized, courierRateController.getCourierRatesByOriginDestination);
+router.get('/courier-rate/:id',authorized, courierRateController.getCourierRateById);
+router.post('/courier-rate',authorized,courierRateController.createCourierRate);
+router.put('/courier-rate/:id',authorized, courierRateController.updateCourierRate);
+router.delete('/courier-rate/:id',authorized, courierRateController.deleteCourierRate);
+
+module.exports = router;
