d800db0e6408d78bddba5f406f9b62c2b2d12779
irvan taufik <irvantaufik28@gmail.com>
2022-12-27 06:34:46 +0700

    fix app

--- logistic_service/src/app.js
+++ logistic_service/src/app.js
@@ -5,15 +5,15 @@ const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
 const serverError = require("./middlerware/serverError")
 
-const LogisticRepository = require('./repository/logisticRepository')
-const LogisticUsecase = require('./usecase/logisticUsecase')
+const CourierRateRepository = require('./repository/courierRateRepository')
+const CourierRateUsecase = require('./usecase/courierRateUsecase')
 
-const logisticRouter = require('./routes/logisticRoutes')
-const logisticUC = new LogisticUsecase(new LogisticRepository)
+const courierRateRouter = require('./routes/courierRateRoutes')
+const courierRateUC = new CourierRateUsecase(new CourierRateRepository)
 
 const app = express();
 app.use((req, res, next) => {
-  req.logisticUC = logisticUC
+  req.courierRateUC = courierRateUC
   next();
 });
 
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1", logisticRouter);
+app.use("/api/v1", courierRateRouter);
 app.use(serverError);
 const swaggerDocument = require('./docs/docs.json');
 app.use(
