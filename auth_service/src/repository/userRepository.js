a597c100c2f82606b41c0b82eeb8093135d8af72
irvan taufik <irvantaufik28@gmail.com>
2022-12-26 14:52:24 +0700

    user repository get user by username or msisdn

---
+++ auth_service/src/repository/userRepository.js
@@ -0,0 +1,19 @@
+const { User } = require("../models");
+const { Op } = require("sequelize");
+
+class UserRepository {
+  constructor() {
+    this._UserModel = User;
+  }
+
+  async getUserByUsernameOrMsisdn(usernameOrMsisdn) {
+    const result = await this._UserModel.findOne({
+      where: {
+        [Op.or]: [{ username: usernameOrMsisdn }, { msisdn: usernameOrMsisdn }],
+      },
+    });
+    return result;
+  }
+}
+
+module.exports = UserRepository;
